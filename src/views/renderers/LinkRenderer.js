3e81e1196ccda13f9df84358589b6c85be685287
IvanProtsenko <ivnprotsenko@gmail.com>
2023-05-25 16:11:42 +0300

    feature: viewed when link is clicked

--- src/views/renderers/LinkRenderer.js
+++ src/views/renderers/LinkRenderer.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { apiService } from '../../services/ApiService';
+
+async function changeViewed(data) {
+    await apiService.updateAdvertByPk({adItemId: data.adItemId, viewed: true})
+}
 
 export default (props) => {
     return (
-        <a target="_blank" href={props.data.link}>{props.data.link}</a>
+        <a onClick={async () => changeViewed(props.data)} target="_blank" href={props.data.link}>{props.data.link}</a>
     );
 };
