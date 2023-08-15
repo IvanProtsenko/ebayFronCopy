fe6967418cd5b14917b338e36bc96c42eecb135b
IvanProtsenko <ivnprotsenko@gmail.com>
2023-08-15 14:00:53 +0300

    refactor: 2 rows in message sender

--- src/views/ConversationFilters/MessageSender.js
+++ src/views/ConversationFilters/MessageSender.js
@@ -51,7 +51,7 @@ export default class MessageSender extends Component {
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Control
               as="textarea"
-              rows={1}
+              rows={2}
               value={this.state.messageText}
               onChange={this.handleInputMessageText}
             />
